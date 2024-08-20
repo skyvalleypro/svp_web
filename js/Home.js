34f1c84f2807712eb864d278b1c66d27f1905a63
skyvalleypro <skyvalleypro@gmail.com>
2024-08-21 03:12:17 +0500

    final commit

---
+++ js/Home.js
@@ -0,0 +1,31 @@
+﻿$(document).on('click', '.ClickNavList', function (event) {
+    event.preventDefault();
+    if ($(".OpenNavList").hasClass("show")) {
+        $(".OpenNavList").removeClass("show");
+    }
+    else {
+        $(".OpenNavList").addClass("show");
+    }
+});
+
+$(document).on('click', '.ClickTogglebtn', function (event) {
+    event.preventDefault();
+    if ($("#navbarNav").hasClass("show")) {
+        $("#navbarNav").removeClass("show");
+    }
+    else {
+        //$("#navbarNav").animate("slow");
+        $("#navbarNav").addClass("show");
+        //$("#navbarNav").animate({ speed: 'slow' });
+    }
+});
+
+//function ClickNavList() {
+//    debugger;
+//    if ($(".OpenNavList").hasClass("show")) {
+//        $(".OpenNavList").removeClass("show");
+//    }
+//    else {
+//        $(".OpenNavList").addClass("show");
+//    }
+//}
